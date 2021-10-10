b9f72ca8d00bd135bc03c3cb3e1a79457fc17da1
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2021-10-10 04:03:52 -0700

    init

--- src/Slider.js
+++ src/Slider.js
@@ -5,6 +5,7 @@ import { Carousel } from 'react-responsive-carousel';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 function Slider() {
     return (
+        <div>
         <div className="slid-im">
             <Carousel infiniteLoop autoPlay >
               <div className="imagees">
@@ -32,8 +33,8 @@ function Slider() {
                  <img className="photo" src="https://ii1.pepperfry.com/media/wysiwyg/banners/WEB_Top_RHS-7-oct.png"/>
              </div> */}
     
-                 <div className="rij">
-                     <div className="mui">
+                 <div className="side-img">
+                     <div className="crash">
                  <img className="photo" src="https://ii1.pepperfry.com/media/wysiwyg/banners/WEB_Top_RHS-7-oct.png"/>
                 </div>
 
@@ -42,11 +43,23 @@ function Slider() {
                 </div>
                 </div>
 
-                <div className="h">
+                <div className="location">
             <span className="symbols"><LocationOnIcon fontSize="large"/></span>
- <span className="sma">Find your Nearest studio</span>
+ <span className="smal">Find your Nearest studio</span>
 </div>
 </div>
+
+    <div className="banner">
+      <div className="banner-one">
+         <img src="https://ii3.pepperfry.com/media/wysiwyg/banners/2021_web_regBanner_01_5Oct.jpg"/>
+          </div> 
+
+          <div className="banner-two">
+         <img src="	https://ii1.pepperfry.com/media/wysiwyg/banners/Safe_Web2_2x_11062021.jpg"/>
+          </div> 
+          </div>
+ </div>
+            
     )
 }
 
